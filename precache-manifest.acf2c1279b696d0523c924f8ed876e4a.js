fa3cbd711e918b0035f7b1ef5a9683a4555e3443
Tevin Sharp <sharp@Tevins-MacBook-Air.local>
2019-11-17 14:50:39 -0600

    Updates

---
+++ precache-manifest.acf2c1279b696d0523c924f8ed876e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c52101b4272c4dbd6ae38cac2394b6b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e376a10bfb616cff3125",
+    "url": "/robofriends/static/css/main.ecc4324b.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "e376a10bfb616cff3125",
+    "url": "/robofriends/static/js/main.d331b726.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
